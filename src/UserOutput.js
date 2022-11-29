e98239b56b42e109bb4a774a9b4660871f4cbfcf
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 14:36:10 +0900

    feat: 구매한 로또를 출력하는 기능

--- src/UserOutput.js
+++ src/UserOutput.js
@@ -4,6 +4,12 @@ const UserOutput = {
   showCount: (money) => {
     Console.print(`${money / 1000}개를 구매했습니다.`);
   },
+
+  showLotto: (lottoArray) => {
+    lottoArray.forEach((lotto) => {
+      Console.print(`${lotto}\n`);
+    });
+  },
 };
 
 module.exports = UserOutput;
