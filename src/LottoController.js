d3c7de3ade35ae1afe7165b2a1708049291ea8d5
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 14:38:14 +0900

    feat(Controller): 구매한 로또 개수를 출력하고 로또를 만들고 보여주는 함수 호출

--- src/LottoController.js
+++ src/LottoController.js
@@ -1,11 +1,20 @@
 const UserInput = require("./UserInput");
 const Validation = require("./Validation");
+const Computer = require("./Computer");
+const UserOutput = require("./UserOutput");
 
 class LottoController {
   gameStart() {
     UserInput.moneyInput((money) => {
       Validation.money(money);
+      UserOutput.showCount(money);
+      this.lottoMakeAndShow(money);
     });
+  }
+
+  lottoMakeAndShow(money) {
+    const lottoArray = Computer(money);
+    UserOutput.showLotto(lottoArray);
   }
 }
 
