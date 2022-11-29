8660fe54281063f05bb4644326671850b350d426
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 15:37:00 +0900

    feat(Controller): 당첨번호를 받고 타당성 검사를 시행하는 함수를 호출

--- src/LottoController.js
+++ src/LottoController.js
@@ -2,19 +2,27 @@ const UserInput = require("./UserInput");
 const Validation = require("./Validation");
 const Computer = require("./Computer");
 const UserOutput = require("./UserOutput");
+const Lotto = require("./Lotto");
 
 class LottoController {
-  gameStart() {
+  getMoneyInput() {
     UserInput.moneyInput((money) => {
       Validation.money(money);
       UserOutput.showCount(money);
       this.lottoMakeAndShow(money);
+      this.getWinningInput();
     });
   }
 
   lottoMakeAndShow(money) {
     const lottoArray = Computer(money);
     UserOutput.showLotto(lottoArray);
+  }
+
+  getWinningInput() {
+    UserInput.winningInput((winningInput) => {
+      const lotto = new Lotto(winningInput);
+    });
   }
 }
 
