bc324cdfef82bb59d08876d53a6760e7ad820dc3
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 19:16:12 +0900

    feat: 판단 결과를 출력하는 기능 추가

--- src/UserOutput.js
+++ src/UserOutput.js
@@ -2,7 +2,7 @@ const { Console } = require("@woowacourse/mission-utils");
 
 const UserOutput = {
   showCount: (money) => {
-    Console.print(`${money / 1000}개를 구매했습니다.`);
+    Console.print(`\n${money / 1000}개를 구매했습니다.`);
   },
 
   showLotto: (lottoArray) => {
@@ -10,6 +10,14 @@ const UserOutput = {
       let splitLotto = lotto.join(`, `).split(`,`);
       Console.print(`[${splitLotto}]`);
     });
+  },
+
+  showResult: (result) => {
+    Console.print(`3개 일치 (5,000원) - ${result.get("5등")}`);
+    Console.print(`4개 일치 (50,000원) - ${result.get("4등")}`);
+    Console.print(`5개 일치 (1,500,000원) - ${result.get("3등")}`);
+    Console.print(`5개 일치 (30,000,000원) - ${result.get("2등")}`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${result.get("1등")}`);
   },
 };
 
