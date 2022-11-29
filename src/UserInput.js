df6d1d02fdc95c81f24630224954e7c21fff3d4e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 14:48:11 +0900

    feat: 당첨번호를 입력받는 함수 추가

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,7 +1,11 @@
 const { Console } = require("@woowacourse/mission-utils");
 const UserInput = {
   moneyInput: (callback) => {
-    Console.readLine(`구입금액을 입력해 주세요.`, callback);
+    Console.readLine(`구입금액을 입력해 주세요.\n`, callback);
+  },
+
+  winningInput: (callback) => {
+    Console.readLint(`당첨 번호를 입력해 주세요.`, callback);
   },
 };
 module.exports = UserInput;
