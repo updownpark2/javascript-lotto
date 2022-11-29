181568a9ffaf01aca0481954d16578446e42387e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 18:18:29 +0900

    refactor: 2등과 3등이 구별 되지 않는 오류 수정 및 하나의 함수에서 모든 계산이 이루어지도록 설계

--- src/LottoJudgement.js
+++ src/LottoJudgement.js
@@ -2,10 +2,6 @@ class LottoJudgement {
   #result = new Map();
   #count = [];
 
-  countReset() {
-    this.#count = 0;
-  }
-
   resultSetting() {
     this.#result.set("5등", 0);
     this.#result.set("4등", 0);
@@ -26,25 +22,22 @@ class LottoJudgement {
     const winningInputArr = this.makeSplitArr(winningInput);
     lottoArray.forEach((lotto) => {
       let win = lotto.filter((lottoNumber) =>
-        winningInputArr.includes(lottoNumber)
+        winningInputArr.includes(String(lottoNumber))
       );
       this.#count.push(win.length);
     });
   }
 
-  bonusCheck(lottoArray, bonusInput) {
-    lottoArray.forEach((lotto) => {
-      if (lotto.includes(bonusInput)) {
-        this.#result.set("2등", this.#result.get("2등") + 1);
-
-        return;
-      }
-      this.#result.set("3등", this.#result.get("3등") + 1);
-    });
+  bonusCheck(lottoArray, bonusInput, index) {
+    if (lottoArray[index].includes(parseInt(bonusInput)) === true) {
+      this.#result.set("2등", this.#result.get("2등") + 1);
+      return;
+    }
+    this.#result.set("3등", this.#result.get("3등") + 1);
   }
 
-  countToResult() {
-    this.#count.forEach((count) => {
+  countToResult(lottoArray, bonusInput) {
+    this.#count.forEach((count, index) => {
       if (count === 3) {
         this.#result.set("5등", this.#result.get("5등") + 1);
       }
@@ -52,14 +45,21 @@ class LottoJudgement {
         this.#result.set("4등", this.#result.get("4등") + 1);
       }
       if (count === 5) {
-        tthis.bonusCheck();
+        this.bonusCheck(lottoArray, bonusInput, index);
       }
       if (count === 6) {
         this.#result.set("1등", this.#result.get("1등") + 1);
       }
     });
   }
+
+  totalCalculation(lottoArray, winningInput, bonusInput) {
+    this.countPush(lottoArray, winningInput);
+    this.resultSetting();
+    this.countToResult(lottoArray, bonusInput);
+  }
 }
+module.exports = LottoJudgement;
 
 //lotto array = [[1,2,3,14,25,36],[7,8,9,10,11,12]]
 // winningInputArr = [1,2,3,4,5,6]
