aca663f255a159677f702bced1fb78f930d25745
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-29 19:34:39 +0900

    feat:계산 된 수익률 출력

--- src/UserOutput.js
+++ src/UserOutput.js
@@ -13,11 +13,15 @@ const UserOutput = {
   },
 
   showResult: (result) => {
-    Console.print(`3개 일치 (5,000원) - ${result.get("5등")}`);
-    Console.print(`4개 일치 (50,000원) - ${result.get("4등")}`);
-    Console.print(`5개 일치 (1,500,000원) - ${result.get("3등")}`);
-    Console.print(`5개 일치 (30,000,000원) - ${result.get("2등")}`);
-    Console.print(`6개 일치 (2,000,000,000원) - ${result.get("1등")}`);
+    Console.print(`3개 일치 (5,000원) - ${result.get("5등")}개`);
+    Console.print(`4개 일치 (50,000원) - ${result.get("4등")}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${result.get("3등")}개`);
+    Console.print(`5개 일치 (30,000,000원) - ${result.get("2등")}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${result.get("1등")}개`);
+  },
+
+  showBenefit: (benefit) => {
+    Console.print(`총 수익률은 ${benefit}%입니다.`);
   },
 };
 
